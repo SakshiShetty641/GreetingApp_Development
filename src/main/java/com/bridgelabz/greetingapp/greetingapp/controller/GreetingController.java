f78f42880430c730c1502dd213592ba6f4c1fa1b
Sakshi <sakshishetty1231997@gmail.com>
2021-10-10 20:32:36 +0530

    Added: Service layer and message

--- src/main/java/com/bridgelabz/greetingapp/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/greetingapp/controller/GreetingController.java
@@ -1,9 +1,18 @@
 package com.bridgelabz.greetingapp.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return greetingService.getMessage();
+    }
 }
