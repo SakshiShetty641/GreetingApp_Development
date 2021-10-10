e95932ffdbecd732eb74dc5e4e6c212d79fed001
Sakshi <sakshishetty1231997@gmail.com>
2021-10-10 20:01:00 +0530

    Added: Greeting Controller to implement the HTTP Methods

---
+++ src/main/java/com/bridgelabz/greetingapp/greetingapp/controller/GreetingController.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.greetingapp.greetingapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+}
